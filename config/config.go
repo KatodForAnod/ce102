d5b3043a8134d603137c0fc07a0086eb28134dea
nikitamakarchuk <kukushkanezabudka@yandex.by>
2022-06-20 22:14:10 +0300

    config added

---
+++ config/config.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+)
+
+type Config struct {
+	ProxyServerAddr string      `json:"proxy_server_addr"`
+	IoTsDevices     []IotConfig `json:"iots_devices"`
+}
+
+type IotConfig struct {
+}
+
+const configPath = "conf.config"
+
+func LoadConfig() (loadedConf Config, err error) {
+	data, err := ioutil.ReadFile(configPath)
+	if err != nil {
+		log.Println(err)
+		return Config{}, err
+	}
+
+	err = json.Unmarshal(data, &loadedConf)
+	return
+}
